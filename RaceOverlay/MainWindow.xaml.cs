4fc6300f75563756c40d78fd8384d9562dc0f32e
ConnorMolz <114417919+ConnorMolz@users.noreply.github.com>
2025-02-15 00:08:50 +0100

    Init IRacing SDK Mapper and Remove that multiple instances of a UI can be invoked

--- RaceOverlay/MainWindow.xaml.cs
+++ RaceOverlay/MainWindow.xaml.cs
@@ -1,30 +1,89 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using HerboldRacing;
 using RaceOverlay.Overlays.Inputs;
 
 namespace RaceOverlay;
 
 /// <summary>
-/// Interaction logic for MainWindow.xaml
+/// Interaction Printic for MainWindow.xaml
 /// </summary>
 public partial class MainWindow : Window
 {
+    public static IRSDKSharper IrsdkSharper = null!;
+
+    private Inputs _inputs;
     public MainWindow()
     {
         InitializeComponent();
+        _initIRacingData();
+        _inputs = new Inputs();
+        
+    }
+
+    private void _initIRacingData()
+    {
+        Debug.Print( "Initializing iRacing data..." );
+        // create an instance of IRSDKSharper
+        IrsdkSharper = new IRSDKSharper();
+
+        // hook up our event handlers
+        IrsdkSharper.OnException += OnException;
+        IrsdkSharper.OnConnected += OnConnected;
+        IrsdkSharper.OnDisconnected += OnDisconnected;
+        IrsdkSharper.OnSessionInfo += OnSessionInfo;
+        IrsdkSharper.OnTelemetryData += OnTelemetryData;
+        IrsdkSharper.OnStopped += OnStopped;
+
+        // this means fire the OnTelemetryData event every 30 data frames (2 times a second)
+        IrsdkSharper.UpdateInterval = 30; 
+
+        // lets go!
+        IrsdkSharper.Start();
     }
     
-    public void Open_Inputs(object sender, RoutedEventArgs e)
+    private void Window_Closing( object sender, CancelEventArgs e )
     {
-        Inputs inputs = new Inputs();
-        inputs.Show();
+        IrsdkSharper.Stop();
+    }
+
+    private void OnException( Exception exception )
+    {
+        Debug.Print( "OnException() fired!" );
+    }
+
+    private void OnConnected()
+    {
+        Debug.Print( "OnConnected() fired!" );
+    }
+
+    private void OnDisconnected()
+    {
+        Debug.Print( "OnDisconnected() fired!" );
+    }
+
+    private void OnSessionInfo()
+    {
+        var trackName = IrsdkSharper.Data.SessionInfo.WeekendInfo.TrackName;
+
+        Debug.Print( $"OnSessionInfo fired! Track name is {trackName}." );
+    }
+
+    private void OnTelemetryData()
+    {
+        var lapDistPct = IrsdkSharper.Data.GetFloat( "CarIdxLapDistPct", 5 );
+
+        Debug.Print( $"OnTelemetryData fired! Lap dist pct for the 6th car in the array is {lapDistPct}." );
+    }
+
+    private static void OnStopped()
+    {
+        Debug.Print( "OnStopped() fired!" );
+    }
+    
+    private void Open_Inputs(object sender, RoutedEventArgs e)
+    {
+        _inputs.Show();
     }
 }
--- RaceOverlay/Overlays/Inputs/Inputs.xaml.cs
+++ RaceOverlay/Overlays/Inputs/Inputs.xaml.cs
@@ -1,14 +1,26 @@
 using System.Windows;
 using System.Windows.Media;
+using HerboldRacing;
 
 namespace RaceOverlay.Overlays.Inputs;
 
 public partial class Inputs : Window
 {
+    
+    private double _throttle;
+    private double _brake;
+    private double _clutch;
+    private int _gear;
+    private double _speed;
+    
+    IRSDKSharper IrsdkSharper = null!;
+    
     public Inputs()
     {
         InitializeComponent();
         DrawHardcodedData();
+        IrsdkSharper = MainWindow.IrsdkSharper;
+        _getData();
     }
 
     private void DrawHardcodedData()
@@ -56,5 +68,10 @@ public partial class Inputs : Window
                 100 - points[i].Y
             );
         }
+    }
+
+    private void _getData()
+    {
+        Console.WriteLine(IrsdkSharper.Data.TelemetryDataProperties.Capacity);
     }
 }
