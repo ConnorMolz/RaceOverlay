d8df37622f87bb1ce1be9a4e799d97d8ee363786
ConnorMolz <114417919+ConnorMolz@users.noreply.github.com>
2025-02-28 22:18:38 +0100

    Implement moving and that on press x the window is only calling Hide()

--- RaceOverlay/Internals/Overlay.cs
+++ RaceOverlay/Internals/Overlay.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Input;
 
@@ -7,6 +8,7 @@ public abstract class Overlay: Window
 {
      public String OverlayName { get; set; }
      public String OverlayDescription { get; set; }
+     public bool PositionIsLocked { get; set; } = true;
      
      public virtual void _updateWindow(){}
      public virtual void _getData(){}
@@ -37,8 +39,26 @@ public abstract class Overlay: Window
           // Check if F7 key was pressed
           if (e.Key == Key.F7)
           {
-               Console.WriteLine($"F7 pressed in overlay: {OverlayName}");
-               // You can add additional actions here
+               Debug.WriteLine($"F7 pressed in overlay: {OverlayName}");
+               TogglePositionLock();
           }
+     }
+     
+     private void TogglePositionLock()
+     {
+          if(PositionIsLocked)
+          {
+               WindowStyle = WindowStyle.SingleBorderWindow;
+               PositionIsLocked = false;
+               return;
+          }
+          WindowStyle = WindowStyle.None;
+          PositionIsLocked = true;
+     }
+
+     protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+     {
+          e.Cancel = true;
+          Hide();
      }
 }
