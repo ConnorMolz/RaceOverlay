76844b5a07d1b3692b30585c1c3de54fcedfec93
ConnorMolz <114417919+ConnorMolz@users.noreply.github.com>
2025-03-02 00:59:01 +0100

    Implement Data Operations

--- RaceOverlay/Overlays/EnergyInfo/EnergyInfo.xaml.cs
+++ RaceOverlay/Overlays/EnergyInfo/EnergyInfo.xaml.cs
@@ -1,23 +1,36 @@
 using System.Windows;
+using RaceOverlay.Data.Models;
 using RaceOverlay.Internals;
 
 namespace RaceOverlay.Overlays.EnergyInfo;
 
 public partial class EnergyInfo : Overlay
 {
+    private float _energyLevelPct;  
+    
+    private iRacingData _data;
+    
+    
     public EnergyInfo(): base("Engery Info", "Displays the current energy level of the battery. (Only available in GPT)")
     {
         InitializeComponent();
+        
+        Thread updateThread = new Thread(UpdateThreadMethod);
+        
+        updateThread.IsBackground = true;
+        updateThread.Start();
     }
 
     public override void _updateWindow()
     {
-        
+        EnergyPctText.Text = _energyLevelPct.ToString("F1") + "%";
+        EnergyBar.Width = _energyLevelPct * 2;
     }
     
     public override void _getData()
     {
-        
+        _data = MainWindow.IRacingData;
+        _energyLevelPct = _data.LocalCarTelemetry.EngeryLevelPct;
     }
     
     public override void UpdateThreadMethod()
