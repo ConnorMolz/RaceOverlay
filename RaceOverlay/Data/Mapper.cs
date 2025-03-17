1f38ce9c85a38267dd806ca876fdfad9f59a82b4
ConnorMolz <114417919+ConnorMolz@users.noreply.github.com>
2025-03-17 23:35:44 +0100

    Try to implement Driver data mapper

--- RaceOverlay/Data/Mapper.cs
+++ RaceOverlay/Data/Mapper.cs
@@ -218,7 +218,37 @@ public class Mapper
         Debug.WriteLine(data.Pitstop.InPit);
         
         // Map Driver Data
+        List<DriverModel> drivers = new List<DriverModel>();
+        try
+        {
+            for (int i = 0; i < irsdkSharper.Data.SessionInfo.DriverInfo.Drivers.Count; i++)
+            {
+                drivers.Add(
+                    new DriverModel(
+                        irsdkSharper.Data.SessionInfo.DriverInfo.Drivers.ElementAt(i).UserName,
+                        irsdkSharper.Data.SessionInfo.DriverInfo.Drivers.ElementAt(i).IRating,
+                        irsdkSharper.Data.SessionInfo.DriverInfo.Drivers.ElementAt(i).LicString,
+                        irsdkSharper.Data.SessionInfo.DriverInfo.Drivers.ElementAt(i).CarNumberRaw,
+                        irsdkSharper.Data.SessionInfo.DriverInfo.Drivers.ElementAt(i).CarClassShortName,
+                        irsdkSharper.Data.SessionInfo.DriverInfo.Drivers.ElementAt(i).CarScreenNameShort,
+                        irsdkSharper.Data.SessionInfo.DriverInfo.Drivers.ElementAt(i).CarIdx,
+                        irsdkSharper.Data.GetFloat("CarIdxLapDistPct", irsdkSharper.Data.SessionInfo.DriverInfo.Drivers.ElementAt(i).CarIdx) *100,
+                        irsdkSharper.Data.SessionInfo.DriverInfo.Drivers.ElementAt(i).CarClassEstLapTime,
+                        irsdkSharper.Data.GetInt("CarIdxPosition", irsdkSharper.Data.SessionInfo.DriverInfo.Drivers.ElementAt(i).CarIdx),
+                        irsdkSharper.Data.GetInt("CarIdxClassPosition", irsdkSharper.Data.SessionInfo.DriverInfo.Drivers.ElementAt(i).CarIdx),
+                        irsdkSharper.Data.GetInt("CarIdxLap", irsdkSharper.Data.SessionInfo.DriverInfo.Drivers.ElementAt(i).CarIdx),
+                        irsdkSharper.Data.GetFloat("CarIdxLastLapTime", irsdkSharper.Data.SessionInfo.DriverInfo.Drivers.ElementAt(i).CarIdx),
+                        irsdkSharper.Data.GetFloat("CarIdxBestLapTime", irsdkSharper.Data.SessionInfo.DriverInfo.Drivers.ElementAt(i).CarIdx)
+                    )
+                );
+            }
+        }
+        catch (Exception e)
+        {
+            //ignored
+        }
         
+        data.Drivers = drivers.ToArray();
         
         // Return Dataset
         return data;
--- RaceOverlay/Data/Models/DriverModel.cs
+++ RaceOverlay/Data/Models/DriverModel.cs
@@ -6,7 +6,7 @@ public class DriverModel
     public int iRating { get; set; }
     public string License {get; set;}
     public int CarNumber { get; set; }
-    public int CarClass { get; set; }
+    public string CarClass { get; set; }
     public string CarName { get; set; }
     
     public int Idx { get; set; }
@@ -16,10 +16,10 @@ public class DriverModel
     public int Position { get; set; }
     public int ClassPosition { get; set; }
     public int Lap { get; set; }
-    public int LastLap { get; set; }
-    public int BestLap { get; set; }
+    public float LastLap { get; set; }
+    public float BestLap { get; set; }
 
-    public DriverModel(string name, int iRating, string license, int carNumber, int carClass, string carName, int idx, float lapDistance, float estCarClassNeededLapTime, int position, int classPosition, int lap, int lastLap, int bestLap)
+    public DriverModel(string name, int iRating, string license, int carNumber, string carClass, string carName, int idx, float lapDistance, float estCarClassNeededLapTime, int position, int classPosition, int lap, float lastLap, float bestLap)
     {
         Name = name;
         this.iRating = iRating;
