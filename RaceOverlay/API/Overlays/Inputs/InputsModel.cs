d2bd9abc57164f2681785ec5a4785ad6d8d1a2aa
ConnorMolz <114417919+ConnorMolz@users.noreply.github.com>
2025-03-21 13:05:22 +0100

    Implements data getting

--- RaceOverlay/API/Overlays/Inputs/InputsModel.cs
+++ RaceOverlay/API/Overlays/Inputs/InputsModel.cs
@@ -1,3 +1,6 @@
+using RaceOverlay;
+using RaceOverlay.Data.Models;
+
 namespace API_test.Overlays.Inputs;
 
 public class InputsModel
@@ -8,21 +11,13 @@ public class InputsModel
     public int Gear { get; set; }
     public double Speed { get; set; }
     
-    public InputsModel(double throttle, double brake, double clutch, int gear, double speed)
-    {
-        Throttle = throttle;
-        Brake = brake;
-        Clutch = clutch;
-        Gear = gear;
-        Speed = speed;
-    }
-    
     public InputsModel()
     {
-        Throttle = 0;
-        Brake = 0;
-        Clutch = 0;
-        Gear = 0;
-        Speed = 0;
+        iRacingData IRacingData = MainWindow.IRacingData;
+        Throttle = IRacingData.Inputs.Throttle;
+        Brake = IRacingData.Inputs.Brake;
+        Clutch = IRacingData.Inputs.Clutch;
+        Gear = IRacingData.LocalCarTelemetry.Gear;
+        Speed = IRacingData.LocalCarTelemetry.Speed;
     }
 }
