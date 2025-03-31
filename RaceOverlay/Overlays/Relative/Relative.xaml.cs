e995980119dc4c3fb7791efb0564e970ba416f0f
ConnorMolz <114417919+ConnorMolz@users.noreply.github.com>
2025-03-31 11:25:02 +0200

    Finish Relative

--- RaceOverlay/Overlays/Relative/Relative.xaml.cs
+++ RaceOverlay/Overlays/Relative/Relative.xaml.cs
@@ -12,6 +12,13 @@ public partial class Relative : Overlay
     private iRacingData _data;
     private List<DriverModel> _driverModels;
     private int _additionalDrivers;
+    private double _timeLeft;
+    private double _timeTotal;
+    private int _lapsLeft;
+    private int _lapsTotal;
+    private int _lapsLeftEstimated;
+    private int _maxIncidents;
+    private int _incidents;
     
     
     //TODO: Add Description
@@ -22,16 +29,47 @@ public partial class Relative : Overlay
 
     public override void _updateWindow()
     {
-        for (int i = 0; i < _driverModels.Count; i++)
+        // Update the Header
+        // 32767 is the default value for lapsTotal when the session is not using laps for the distance
+        if (_lapsTotal == 32767)
         {
-            Body.Children.Add(new RelativeRow(
-                _driverModels[i].Name,
-                _driverModels[i].Position,
-                Math.Abs(_data.GetGapToPlayerMs(_driverModels[i].Idx)),
-                _driverModels[i].CarNumber,
-                _driverModels[i].ClassColorCode
-            ));
+            // Time Formatting
+            // 00:00:00 / 00:00:00 ~ 0/0 Laps
+            TimeSpan timeLeft = TimeSpan.FromSeconds(_timeLeft);
+            TimeSpan timeTotal = TimeSpan.FromSeconds(_timeTotal);
+            TimeOrLaps.Text = $"{timeLeft:hh\\:mm\\:ss} / {timeTotal:hh\\:mm\\:ss}";
         }
+        
+        
+        if (_lapsTotal != 32767)
+        {
+            // Lap Formatting
+            // 0/0 Laps
+            TimeOrLaps.Text = $"{_lapsTotal - _lapsLeft}/{_lapsTotal} Laps";
+        }
+        
+        // Incident Formating
+        IncidentsText.Text = $"X: {_incidents}/{_maxIncidents}";
+
+        try
+        {
+            Body.Children.Clear();
+            for (int i = 0; i < _driverModels.Count; i++)
+            {
+                Body.Children.Add(new RelativeRow(
+                    _driverModels[i].Name,
+                    _driverModels[i].Position,
+                    Math.Abs(_data.GetGapToPlayerMs(_driverModels[i].Idx)),
+                    _driverModels[i].CarNumber,
+                    _driverModels[i].ClassColorCode
+                ));
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine(e);
+        }
+       
     }
 
     public override void _getData()
@@ -40,6 +78,22 @@ public partial class Relative : Overlay
         if (_data == null)
             return;
         _driverModels = GetRelative();
+        _timeLeft = _data.SessionData.TimeLeft;
+        _timeTotal = _data.SessionData.TimeTotal;
+        _lapsLeft = _data.SessionData.LapsLeft;
+        _lapsTotal = _data.SessionData.LapsTotal;
+        _lapsLeftEstimated = _data.SessionData.LapsLeftEstimated;
+        _maxIncidents = _data.SessionData.MaxIncidents;
+        _incidents = _data.SessionData.Incidents;
+
+        if (!_devMode)
+        {
+            InCar = _data.InCar;
+        }
+        else
+        {
+            InCar = true;
+        }
     }
 
     public override void UpdateThreadMethod()
