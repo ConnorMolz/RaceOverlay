b1698c84bb8181b4d749e35fa76384be595b70c0
ConnorMolz <114417919+ConnorMolz@users.noreply.github.com>
2025-04-08 15:16:30 +0200

    Fix Col sizes

--- RaceOverlay/Overlays/Relative/RelativeRow.cs
+++ RaceOverlay/Overlays/Relative/RelativeRow.cs
@@ -24,14 +24,28 @@ public class RelativeRow: Grid
         
         RowDefinitions.Add(new RowDefinition());
         
-        ColumnDefinitions.Add(new ColumnDefinition());
-        ColumnDefinitions.Add(new ColumnDefinition());
-        ColumnDefinitions.Add(new ColumnDefinition());
-        ColumnDefinitions.Add(new ColumnDefinition());
+        ColumnDefinition col1 = new ColumnDefinition();
+        col1.Width = GridLength.Auto;
+        
+        ColumnDefinition col2 = new ColumnDefinition();
+        col2.Width = GridLength.Auto;
+        
+        ColumnDefinition col3 = new ColumnDefinition();
+        col3.Width = GridLength.Auto;
+        
+        ColumnDefinition col4 = new ColumnDefinition();
+        col4.Width = GridLength.Auto;
+        
+        ColumnDefinitions.Add(col1);
+        ColumnDefinitions.Add(col2);
+        ColumnDefinitions.Add(col3);
+        ColumnDefinitions.Add(col4);
         
         TextBlock positionTextBlock = new TextBlock();
-        positionTextBlock.Text = Position.ToString();
+        positionTextBlock.Text = Position + ".";
         positionTextBlock.Background = _getClassColorBrush(classColorCode.Replace("0x", "#"));
+        positionTextBlock.Width = 15;
+        positionTextBlock.Margin = new Thickness(0, 0, 5, 0);
         
         positionTextBlock.SetValue(Grid.ColumnProperty, 0);
         positionTextBlock.SetValue(Grid.RowProperty, 0);
@@ -39,7 +53,9 @@ public class RelativeRow: Grid
         
         
         TextBlock carNrTextBlock = new TextBlock();
-        carNrTextBlock.Text = CarNr.ToString();
+        carNrTextBlock.Text = "#" + CarNr;
+        carNrTextBlock.Width = 15 + CarNr.ToString().Length * 9;
+        carNrTextBlock.Margin = new Thickness(0, 0, 5, 0);
 
         carNrTextBlock.SetValue(Grid.ColumnProperty, 1);
         carNrTextBlock.SetValue(Grid.RowProperty, 0);
@@ -48,6 +64,8 @@ public class RelativeRow: Grid
         
         driverNameTextBlock = new TextBlock();
         driverNameTextBlock.Text = DriverName;
+        driverNameTextBlock.Width = 15 + DriverName.ToString().Length * 9;
+        driverNameTextBlock.Margin = new Thickness(0, 0, 5, 0);
         
         driverNameTextBlock.SetValue(Grid.ColumnProperty, 2);
         driverNameTextBlock.SetValue(Grid.RowProperty, 0);
@@ -57,6 +75,7 @@ public class RelativeRow: Grid
         TextBlock distanceTextBlock = new TextBlock();
         distanceTextBlock.Text = TimeSpan.FromMilliseconds(distance).ToString(@"ss\.f");
         distanceTextBlock.TextAlignment = TextAlignment.Right;
+        distanceTextBlock.Margin = new Thickness(0, 0, 5, 0);
         
         distanceTextBlock.SetValue(Grid.ColumnProperty, 3);
         distanceTextBlock.SetValue(Grid.RowProperty, 0);
