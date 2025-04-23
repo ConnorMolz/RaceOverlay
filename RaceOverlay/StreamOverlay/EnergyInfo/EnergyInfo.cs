46d11fdb8d21efa08e32bf348da91cfbb962934d
ConnorMolz <conn-or@gmx.de>
2025-04-23 22:58:51 +0200

    Complete EnergyInfo

--- RaceOverlay/StreamOverlay/EnergyInfo/EnergyInfo.cs
+++ RaceOverlay/StreamOverlay/EnergyInfo/EnergyInfo.cs
@@ -2,7 +2,9 @@ namespace RaceOverlay.StreamOverlay.EnergyInfo;
 
 public class EnergyInfo: Internals.StreamOverlay
 {
-    public EnergyInfo(): base("Energy Info", "", "http://localhost:5480/overlay/energy_info")
+    public EnergyInfo(): base("Energy Info", 
+        "Displays the current energy level of the battery. (Only available in GPT)", 
+        "http://localhost:5480/overlay/energy_info")
     {
         
     }
